44eb52ed71255ce9a7bb8ebc5d7a9c80cf1eeddb
Happy.Shandilya <happyshandilya@gmail.com>
2017-09-26 14:49:07 -0600

    email notification

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.hello, name='hello')
+    # url(r'^$', views.hello, name='hello'),
+    url(r'^$', views.send_email, name='send_email'),
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -2,9 +2,27 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.core.mail import send_mail, BadHeaderError
+
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
-def hello(request):
-    return HttpResponse("Happy I am inside the app now")
+
+# def hello(request):
+#     return HttpResponse("Happy I")
+
+def send_email(request):
+    subject = request.POST.get('subject', 'hullalal')
+    message = request.POST.get('message', 'hey there')
+    from_email = request.POST.get('happyshandilya@gmail.com', 'ok')
+    if subject and message and from_email:
+        try:
+            send_mail(subject, message, from_email, ['happyshandilya@gmail.com'])
+        except BadHeaderError:
+            return HttpResponse('Invalid header found.')
+        return HttpResponseRedirect('/contact/thanks/')
+    else:
+        # In reality we'd use a form class
+        # to get proper validation errors.
+        return HttpResponse('Make sure all fields are entered and valid.')
